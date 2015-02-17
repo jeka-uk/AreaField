6a60b0df5153c05d5676822a88729420a29a91e9
jane_uk@inbox.ru <jane_uk@inbox.ru>
2015-02-17 17:18:46 +0200

    up

---
+++ AreaFields/src/com/example/areafields/fragment/MainActivityFragment.java
@@ -0,0 +1,21 @@
+package com.example.areafields.fragment;
+
+import com.example.areafields.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivityFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+		return view;
+	}
+
+}
